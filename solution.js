a33f625010e94bbcfaa4f1630c1754875e853995
Dheeraj Singh <dhrax21bc@gmail.com>
2022-12-16 09:47:51 +0530

    final

--- solution.js
+++ solution.js
@@ -7,9 +7,9 @@ function palindrome(str){
        for (let i = 0; i < len / 2; i++) {
 
            if (str[i] !== str[len - 1 - i]) {
-               return 'false';
+               console.log(false);
            }
        }
-       return 'true';
+       return console.log(true);
 }
 module.exports = palindrome
